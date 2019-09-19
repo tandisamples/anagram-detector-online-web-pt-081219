44a52b32f9930d6fadb938dedf639ccf8b1f70a1
Tandi Samples <tandisamples@gmail.com>
2019-09-19 00:25:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end 
+
+  def match(array)
+
+    letters_in_word = array.collect{|word|word.split("")}
+    matches = letters_in_word.find_all{|letters|letters.sort == @word.split("").sort}
+    matches.collect{|letters|letters.join("")}
+    #return all matches in array or an empty array if no matches
+  end 
+
+
+end 
